8e3bb673e85b2a8711c31fa0d07eabc320f804d9
unknown <suhasinigupta31@gmail.com>
2023-05-09 23:40:58 +0530

    Added templates

--- gemstone/logger.py
+++ gemstone/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 ROOT_DIR= os.getcwd()
-FILE_NAME= f"{datetime.now().strftime('%Y-%M-%d_%H-%M-%S')}.log"
+FILE_NAME= f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 FILE_PATH= os.path.join(ROOT_DIR,"logs", FILE_NAME)
 os.makedirs(os.path.dirname(FILE_PATH), exist_ok= True)
  
